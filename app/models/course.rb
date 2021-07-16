9d128f18dd1c7fb53e925e5163b5964ba5e087b4
Ubuntu <ubuntu@ip-172-31-22-141.ap-northeast-1.compute.internal>
2021-07-16 06:47:42 +0000

    add friendlyid

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,19 @@
 class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { :minimum => 5}
+  # attr_accessor :slug
+  
+  extend FriendlyId
+  friendly_id :title, use: [:slugged]
+  
+  ## assign random id instead
+  # friendly_id :generated_slug, use: :slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
+  
+  
   def to_s
     title
   end
