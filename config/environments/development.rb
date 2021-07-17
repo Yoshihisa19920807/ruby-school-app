d1a3ce93cc853cd604ae2fbc8b3c4266b815c413
Yoshihisa19920807 <yoshi19920807@gmail.com>
2021-07-17 16:08:51 +0900

    mail config for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,5 +76,16 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "yoshi19920807@gmail.com", 
+    :password => "wbzqxbstvmfairbc", 
+    :authentication => 'login'
+}
 
 end
