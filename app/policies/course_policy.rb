1ad492241cf0ce3b19c5ab134ba5a8588a1e08a4
Ubuntu <ubuntu@ip-172-31-22-141.ap-northeast-1.compute.internal>
2021-07-26 02:14:05 +0000

    bugfix for pundit

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -23,23 +23,23 @@ class CoursePolicy < ApplicationPolicy
     # p @user
     # p current_user
       # if user doesn't have admin role and the creater of the target course isn't the current_user , it raises an error
-    @user.has_role?:admin || @record.user == @user if @user.present?
+    @user.has_role?(:admin) || @record.user == @user if @user.present?
   end
   
   def update?
-    @user.has_role?:admin || @record.user == @user if @user.present?
+    @user.has_role?(:admin) || @record.user == @user if @user.present?
   end
   
   def create?
-    @user.has_role?:teacher if @user.present?
+    @user.has_role?(:teacher) if @user.present?
   end
   
   def new?
-    @user.has_role?:teacher if @user.present?
+    @user.has_role?(:teacher) if @user.present?
   end
   
   def destroy?
-    @user.has_role?:admin || @record.user == @user if @user.present?
+    @user.has_role?(:admin) || @record.user == @user if @user.present?
   end
   
 end
--- app/policies/lesson_policy.rb
+++ app/policies/lesson_policy.rb
@@ -5,6 +5,10 @@ class LessonPolicy < ApplicationPolicy
     end
   end
   
+  def show?
+    @user.has_role?(:admin) || @record.course.user_id == @user.id
+  end
+  
   def edit?
     # defined in application_policy.rb
     # p "____@record"
@@ -19,23 +23,23 @@ class LessonPolicy < ApplicationPolicy
     # p @user
     # p current_user
       # if user doesn't have admin role and the creater of the target course isn't the current_user , it raises an error
-    @user.has_role?:admin || @record.user == @user if @user.present?
+    @user.has_role?(:admin) || @record.course.user == @user if @user.present?
   end
   
   def update?
-    @user.has_role?:admin || @record.user == @user if @user.present?
+    @user.has_role?(:admin) || @record.course.user == @user if @user.present?
   end
   
   def create?
-    @user.has_role?:teacher if @user.present?
+    @user.has_role?(:teacher) if @user.present?
   end
   
   def new?
-    @user.has_role?:teacher if @user.present?
+    @user.has_role?(:teacher) if @user.present?
   end
   
   def destroy?
-    @user.has_role?:admin || @record.user == @user if @user.present?
+    @user.has_role?(:admin) || @record.course.user == @user if @user.present?
   end
   
 end
