2fa4ad7090aba0677f858558cff818b46af4984f
Yoshihisa Okada <yoshi19920807@gmail.com>
2021-08-02 10:51:33 +0900

    add scopes to courses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,21 +2,15 @@ class HomeController < ApplicationController
   # skip authentication on top page
   skip_before_action :authenticate_user!, :only => [:index]
   def index
-    @courses = Course.all.limit(3)
-    @latest_courses = Course.all.limit(3).order(created_at: :desc)
-    @activities = PublicActivity::Activity.all
-    # p "@activities"
-    # p @activities
-    # p "@activities.first.course.create"
-    # p @activities.first.key
+    @latest_courses = Course.latest
+    @top_rated_courses = Course.top_rated
+    @popular_courses = Course.popular
+    # @activities = PublicActivity::Activity.all
   end
   
   # activity viewを表示時に読み込まれる
   def activity
-    p "activity_____"
     @activities = PublicActivity::Activity.all
-    p "@activities"
-    p @activities
     p "@activities.second.trackable"
     p @activities.second.trackable
   end
