8d90c78737dd588af28fef464ffa251c99ca2a16
Yoshihisa Okada <yoshi19920807@gmail.com>
2021-08-04 09:26:39 +0900

    add namescapce charts in route

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,21 @@
 class ChartsController < ApplicationController
-    def users_per_day
-        render json: User.group_by_day(:created_at).count
-    end
+  def users_per_day
+    render json: User.group_by_day(:created_at).count
+  end
 
-    def enrollments_per_day
-        render json: Enrollment.group_by_day(:created_at).count
-    end
+  def enrollments_per_day
+    render json: Enrollment.group_by_day(:created_at).count
+  end
 
-    def courses_per_day
-        render json: Course.group_by_day(:created_at).count
-    end
+  def courses_per_day
+    render json: Course.group_by_day(:created_at).count
+  end
+
+  def course_popularity
+    render json: Enrollment.joins(:course).group(:'courses.title').count
+  end
+
+  def money_makers
+    render json: Enrollment.joins(:course).group(:'courses.title').sum(:price)
+  end
 end
