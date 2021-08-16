9299abe0f583c32e67fce523eafc29106f5aeb9c
Yoshihisa Okada <yoshi19920807@gmail.com>
2021-08-16 16:01:00 +0900

    google omniauth tmp

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+         :recoverable, :rememberable, :validatable,
+         :trackable, :confirmable,:omniauthable , omniauth_providers: [:google_oauth2]
 
   has_many :courses, dependent: :nullify
   has_many :enrollments, dependent: :nullify
@@ -37,6 +38,20 @@ class User < ApplicationRecord
       self.add_role(:teacher) #if you want any user to be able to create own courses
     end
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data['name'],
+    #        email: data['email'],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+end
 
   private
 
